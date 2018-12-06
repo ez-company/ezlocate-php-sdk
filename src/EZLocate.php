4a2cf0c6d0b8e14358352aff654e7cd32d2b984a
Jovanni Lo <lodev09@users.noreply.github.com>
2018-12-07 02:47:04 +0800

    Update EZLocate.php

--- src/EZLocate.php
+++ src/EZLocate.php
@@ -7,11 +7,12 @@ use \Common\Util;
 
 class EZLocate {
 
-    const API_URL = 'https://ezlocate.app/api';
-
     private $_curl;
+    private $_api_url;
 
-    public function __construct($usernname, $password) {
+    public function __construct($usernname, $password, $url = 'https://ezlocate.app/api') {
+        $this->_api_url = $url;
+
         $this->_curl = new Curl();
         $this->_curl->setBasicAuthentication($usernname, $password);
         $this->_curl->setHeader('Content-Type', 'application/json');
@@ -27,7 +28,7 @@ class EZLocate {
      *
      */
     public function createOrder($data) {
-        $response = $this->_curl->post(self::API_URL.'/orders', $data);
+        $response = $this->_curl->post($this->_api_url.'/orders', $data);
         if ($this->_curl->error) {
             throw new ProtocolException($response, $this->_curl);
             return false;
@@ -46,12 +47,33 @@ class EZLocate {
      *
      */
     public function getOrder($id) {
-        $response = $this->_curl->get(self::API_URL.'/orders/'.$id);
+        $response = $this->_curl->get($this->_api_url.'/orders/'.$id);
         if ($this->_curl->error) {
             throw new ProtocolException($response, $this->_curl);
             return false;
         } else {
             return new Order($response);
+        }
+    }
+
+    /**
+     * Get orders
+     *
+     * @return array
+     *
+     */
+    public function getOrders() {
+        $response = $this->_curl->get($this->_api_url.'/orders/'.$id);
+        if ($this->_curl->error) {
+            throw new ProtocolException($response, $this->_curl);
+            return false;
+        } else {
+            $orders = [];
+            foreach ($response as $order_data) {
+                $orders[] = new Order($order_data);
+            }
+
+            return $orders;
         }
     }
 }
