9bdfd5b1476e7e1aaac1e3fb0d403e7dc04cf880
Jovanni Lo <lodev09@users.noreply.github.com>
2018-12-07 05:57:39 +0800

    Update ProtocolException.php

--- src/ProtocolException.php
+++ src/ProtocolException.php
@@ -5,8 +5,8 @@ namespace EZLocate;
 class ProtocolException extends \Exception {
 
     public function __construct($response, $curl) {
-    	$message = is_string($response) ? $response : $response->message;
-        $message = 'Error: ' . $curl->errorCode . ': ' . $curl->errorMessage.'. Message: '.$message;
+    	$message = is_object($response) ? $response->message : $response;
+        $message = 'Error: ' . $curl->errorCode . ': ' . $curl->errorMessage.'. Message: '.($message ? : 'Unknown error');
         parent::__construct($message);
     }
 }
